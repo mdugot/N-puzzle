e40e697dab357ad2a73ec93fc9117e173bd8afbb
Florence Dugot-Duhamel <merlin.dugot@gmail.com>
2018-01-24 20:20:37 +0900

    gestion ctrl-c

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import parser
 from solver import Solver
 
-if __name__ == "__main__":
-    solver = Solver(parser.parser)
-    solver.start()
+try:
+	if __name__ == "__main__":
+	    solver = Solver(parser.parser)
+	    solver.start()
+except (KeyboardInterrupt):
+	solver.sayGoodbye()
